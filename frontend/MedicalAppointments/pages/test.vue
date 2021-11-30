eef7a2223097e68b7ee351c28de28dd5b96de935
Manubilo <mjdelgado@pucp.pe>
2021-11-30 10:58:06 -0500

    First Setup

---
+++ frontend/MedicalAppointments/pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Name: {{ fullName }} Message : {{ message }}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      message: "Hello World",
+      firstName: "Manuel",
+      lastName: "Delgado",
+    };
+  },
+  computed: {
+    fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+});
+</script>
+
+<style scoped></style>
